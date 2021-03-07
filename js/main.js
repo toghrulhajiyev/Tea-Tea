3d68bf467b9b948480398ffea5f3ff18fe751ac8
toghrulhajiyev <togrul.haciyev24@gmail.com>
2021-03-07 14:09:22 +0400

    Products and Quality sections are done

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// Arrivals Carousel
 $(document).ready(function() {
+    // Arrivals Carousel
     $('.owl-carousel').owlCarousel({
         loop:true,
         margin:30,
@@ -13,12 +13,50 @@ $(document).ready(function() {
                 items:1
             },
             600:{
-                items:3
+                items:2
             },
             1000:{
                 items:2
             }
         }
     });
-});
 
+    // Products Slider
+$('.products-container').slick({
+    dots: true,
+    infinite: false,
+    speed: 500,
+    prevArrow: false,
+    nextArrow: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: false,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          dots: false,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+
+});
